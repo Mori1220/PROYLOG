f7610dbb04bdfbbca8af6d2bc82a383ba72f91eb
Mori1220 <moriAeseces@gmail.com>
2022-12-02 22:30:33 -0600

    fix: udpated enviromentt.prod to resolve build problems

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCBNoFLFW7akseNifo_ThHD1sHeM7uXMY8",
+    authDomain: "proylog-1.firebaseapp.com",
+    projectId: "proylog-1",
+    storageBucket: "proylog-1.appspot.com",
+    messagingSenderId: "822801245291",
+    appId: "1:822801245291:web:a946507261d433c4cc7d2e"
+  }
 };
